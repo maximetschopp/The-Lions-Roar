654ee097e49c0cf507571b66c5489e1f542ee96d
Maxime Tschopp <maxime.tschopp@icloud.com>
2022-09-06 16:46:24 +0200

    fully generate a publication, no JSON parsing

--- loadPublication.js
+++ loadPublication.js
@@ -24,21 +24,61 @@ function addPublication(year, month, week, isAfter){
     var publicationContainer = document.createElement("div");
     publicationContainer.classList.add("publication-container");
 
+    // Create PUBLICATION GRID / MAIN ARTICLE
+    // PUBLICATION GRID includes the icon and the description
+    var publicationGrid = generateMainArticle("Title", "Date", "https://www.youtube.com/watch?v=HLzq3NDCdvU", "video", null);
+
+    // Create EXTRA ARTICLES
+    // EXTRA ARTICLES contains a list of extra articles
+    var extraArticlesGrid = document.createElement('div');
+    extraArticlesGrid.className = 'extra-articles-grid';
+
+    for (let numArticles = 0; numArticles < 2; numArticles++) {      /// GET THE NUMBER OF EXTRA ARTICLES
+        //title, author, date, url, type, thumbnail, tags
+        extraArticlesGrid.appendChild(generateArticle("Name of the article", "Maxime Tschopp", "21 Sep", "https://www.youtube.com/watch?v=HLzq3NDCdvU", "video", null, []));
+    }
+
+    publicationContainer.appendChild(publicationGrid);
+    publicationContainer.appendChild(extraArticlesGrid);
+    // appends the publication to the end of the document FOR NOW,
+    // isAfter is a bool to see if the publication should be loaded in before
+    // or after the current publication that the user in on (viewing)
+    document.body.appendChild(publicationContainer);
+}
+
+function generateArticleIcon(type, url, thumbnail){
+    if(type == "video"){
+        var vid = document.createElement("div");
+        vid.classList.add("vid");
+        var thumbnail = document.createElement("img");
+        thumbnail.classList.add("thumbnail");
+
+        // gets the thumbnail from the video url
+        var youtube_video_id = url.match(/youtube\.com.*(\?v=|\/embed\/)(.{11})/).pop();
+
+        thumbnail.src = "https://img.youtube.com/vi/" + youtube_video_id + "/0.jpg";
+
+        vid.appendChild(thumbnail);
+        return vid;
+    }
+}
+
+function generateMainArticle(title, date, url, type, thumbnail) {
     // Create PUBLICATION GRID
     // PUBLICATION GRID includes the icon and the description
     var publicationGrid = document.createElement("div");
     publicationGrid.classList.add("publication-grid");
     // add Main Article Icon
-    var articleIcon = generateArticleIcon("video", "https://www.youtube.com/watch?v=HLzq3NDCdvU");
+    var articleIcon = generateArticleIcon(type, url, thumbnail);
     publicationGrid.appendChild(articleIcon);
     // add Main Article Description
     var mainArticleDescriptionContainer = document.createElement("div");
     mainArticleDescriptionContainer.classList.add("main-article-desc-container");
     var mainArticleDescription = document.createElement("div");
     var mainArticleDate = document.createElement("h1");
-    mainArticleDate.innerHTML = "5 Sep";                             /// GET THE DATE OF THE PUBLICATION
+    mainArticleDate.innerHTML = date;                                /// GET THE DATE OF THE PUBLICATION
     var mainArticleTitle = document.createElement("h2");
-    mainArticleTitle.innerHTML = "Title of Main Article here";       /// GET THE TITLE OF MAIN ARTICLE
+    mainArticleTitle.innerHTML = title;                              /// GET THE TITLE OF MAIN ARTICLE
     var extrasButton = document.createElement("div");
     extrasButton.classList.add("ExtrasButton");
     extrasButton.innerHTML = "Extras";
@@ -50,40 +90,36 @@ function addPublication(year, month, week, isAfter){
     mainArticleDescriptionContainer.appendChild(mainArticleDescription);
     publicationGrid.appendChild(mainArticleDescriptionContainer); 
 
-    // Create EXTRA ARTICLES
-    // EXTRA ARTICLES contains a list of extra articles
-    var extraArticlesGrid = document.createElement('div');
-    extraArticlesGrid.className = 'extra-articles-grid';
+    return publicationGrid;
+}
+
+function generateArticle(title, author, date, url, type, thumbnail, tags){
+    var article = document.createElement("div");
+    article.classList.add("extra-article");
+    var articleGrid = document.createElement("div");
+    articleGrid.classList.add("extra-article-grid");
+
+
+    var icon = generateArticleIcon(type, url, thumbnail);
+    articleGrid.appendChild(icon);
+
+    //desc
+    var articleTitle = document.createElement("h3");
+    articleTitle.innerHTML = title;
+    var articleAuthor = document.createElement("h4");
+    articleAuthor.innerHTML = author;
+    var articleDate = document.createElement("h5");
+    articleDate.innerHTML = date;
+
+    var descriptionContainer = document.createElement("div");
+    descriptionContainer.appendChild(articleTitle);
+    descriptionContainer.appendChild(articleAuthor);
+    descriptionContainer.appendChild(articleDate);
+
+    articleGrid.appendChild(descriptionContainer);
+
+
+    article.appendChild(articleGrid);
     
-    for (let numArticles = 0; numArticles < 2; numArticles++) {      /// GET THE NUMBER OF EXTRA ARTICLES
-        generateArticle();
-    }
-
-    publicationContainer.appendChild(publicationGrid);
-    publicationContainer.appendChild(extraArticlesGrid);
-    // appends the publication to the end of the document FOR NOW
-    // isAfter is a bool to see if the publication should be loaded in before
-    // or after the current publication that the user in on (viewing)
-    document.body.appendChild(publicationContainer);
-}
-
-function generateArticleIcon(type, link){
-    if(type == "video"){
-        var vid = document.createElement("div");
-        vid.classList.add("vid");
-        var thumbnail = document.createElement("img");
-        thumbnail.classList.add("thumbnail");
-
-        // gets the thumbnail from the video url
-        var youtube_video_id = link.match(/youtube\.com.*(\?v=|\/embed\/)(.{11})/).pop();
-
-        thumbnail.src = "https://img.youtube.com/vi/" + youtube_video_id + "/0.jpg";
-
-        vid.appendChild(thumbnail);
-        return vid;
-    }
-}
-
-function generateArticle(){
-
+    return article;
 }

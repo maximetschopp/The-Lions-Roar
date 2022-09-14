54c8fb02d2a39f8e7d289fb823c5b3909f3f8276
Maxime Tschopp <maxime.tschopp@icloud.com>
2022-09-14 20:14:56 +0200

    started mobile

---
+++ mobile/m_app.js
@@ -0,0 +1,103 @@
+document.addEventListener("DOMContentLoaded", ready);
+function ready() {
+  console.log("DOMContentLoaded");
+  loadMainArticle();
+}
+
+function loadMainArticle(year, month, week){
+    var mainArticleContainer = document.getElementById("mainArticleContainer");
+    mainArticleContainer.appendChild(generateArticleIcon("video", "https://www.youtube.com/watch?v=b-_yIBuFXyM"));
+}
+
+function generateArticleIcon(type, url, thumbnail) {
+    if (type == "video") {
+        var vidIcon = document.createElement("div");
+        vidIcon.classList.add("video-icon-container");
+
+        if (url != null) {
+            vidIcon.addEventListener(
+                "click",
+                function (url) {
+                    window.open(url);
+                }.bind(this, url)
+            );
+        }
+        var thumbnailImage = document.createElement("img");
+        thumbnailImage.classList.add("thumbnail");
+
+        // checks if there is an alternative thumbnail available
+        // if not, then gets the thumbnail from the video url (youtube servers)
+        if (thumbnail == null) {
+            var youtube_video_id = url
+                .match(/youtube\.com.*(\?v=|\/embed\/)(.{11})/)
+                .pop();
+            thumbnailImage.src =
+                "https://img.youtube.com/vi/" + youtube_video_id + "/0.jpg";
+        } else {
+            thumbnailImage.src = thumbnail;
+        }
+        vidIcon.appendChild(thumbnailImage);
+
+        var centeredIcon = document.createElement("img");
+        centeredIcon.classList.add("centered-icon");
+        centeredIcon.src = "resources/Icons/video_icon_fill.svg";
+        vidIcon.appendChild(centeredIcon);
+
+        return vidIcon;
+    } else if (type == "image") {
+        var imageIcon = document.createElement("div");
+        imageIcon.classList.add("image-icon-container");
+        var thumbnailImage = document.createElement("img");
+        thumbnailImage.classList.add("thumbnail");
+        thumbnailImage.src = thumbnail;
+
+        imageIcon.appendChild(thumbnailImage);
+
+        var centeredIcon = document.createElement("img");
+        centeredIcon.classList.add("centered-icon");
+        centeredIcon.src = "resources/Icons/image_icon_fill.svg";
+        imageIcon.appendChild(centeredIcon);
+
+        return imageIcon;
+    } else if (type == "audio") {
+        var audioIcon = document.createElement("div");
+        audioIcon.classList.add("audio-icon-container");
+
+        var centeredIcon = document.createElement("img");
+        centeredIcon.classList.add("centered-icon");
+        centeredIcon.src = "resources/Icons/audio_icon_fill.svg";
+        audioIcon.appendChild(centeredIcon);
+
+        return audioIcon;
+    } else if (type == "text") {
+        var textIcon = document.createElement("div");
+        textIcon.classList.add("text-icon-container");
+        var thumbnailImage = document.createElement("img");
+        thumbnailImage.classList.add("thumbnail");
+        if (thumbnail == null) {
+            thumbnailImage.src =
+                "resources/default_thumbnails/text_thumbnail.png";
+        } else {
+            thumbnailImage.src = thumbnail;
+        }
+        textIcon.appendChild(thumbnailImage);
+        var centeredIcon = document.createElement("img");
+        centeredIcon.classList.add("centered-icon");
+        centeredIcon.src = "resources/Icons/text_icon_fill.svg";
+        textIcon.appendChild(centeredIcon);
+
+        return textIcon;
+    } // else its a link
+    else if (type == "link" || type == null) {
+        var linkIcon = document.createElement("div");
+        linkIcon.classList.add("link-icon-container");
+
+        var centeredIcon = document.createElement("img");
+        centeredIcon.classList.add("centered-icon");
+        centeredIcon.src = "resources/Icons/link_icon_fill.svg";
+        linkIcon.appendChild(centeredIcon);
+
+        return linkIcon;
+    }
+    console.warn(type + "article type is not supported");
+}

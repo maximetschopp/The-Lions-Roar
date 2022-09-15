1ffeeb7f8ef57a5c79dd0924ac83f1874b1773bc
Maxime Tschopp <maxime.tschopp@icloud.com>
2022-09-15 10:48:25 +0200

    mobile view + white text for st patricks dat

--- color_management.js
+++ color_management.js
@@ -28,6 +28,7 @@ document.addEventListener("DOMContentLoaded", function () {
             "brightness(50%) sepia(1) hue-rotate(90deg) brightness(100%) saturate(200%) drop-shadow(0 0 50px rgba(0, 0, 0, 0.19))";
         sheet.innerHTML = "";
         sheet.innerHTML += "* {--main-bg-color: #65da91;}\n";
+        sheet.innerHTML += "* {--main-text-color: #ffffff;}\n";
         document.body.appendChild(sheet);
         //document.body.style.backgroundColor = "#65da91";
     }

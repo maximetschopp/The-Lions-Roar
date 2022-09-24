2a7a3bc89f5a77eae57da30f60d05492fcae78ee
Maxime Tschopp <maxime.tschopp@icloud.com>
2022-09-24 11:57:33 +0200

    st patricks

--- color_management.js
+++ color_management.js
@@ -24,26 +24,32 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 });
 
-function clearThemes() {}
-
 function stPatricksTheme() {
     console.log("St. Patrick's Day");
+
+    document.getElementById("logo").src = "resources/Logos/TLR_st_patricks.svg";
+
     var sheet = document.createElement("style");
     document.getElementById("organic_shape_l3").style.filter =
-        "brightness(50%) sepia(1) hue-rotate(90deg) brightness(60%) saturate(200%) drop-shadow(0 0 50px rgba(0, 0, 0, 0.19))";
+    "brightness(50%) sepia(1) hue-rotate(90deg) brightness(80%) saturate(180%) drop-shadow(0 0 50px rgba(0, 0, 0, 0.19))";
     document.getElementById("organic_shape_l2").style.filter =
-        "brightness(50%) sepia(1) hue-rotate(90deg) brightness(80%) saturate(200%) drop-shadow(0 0 50px rgba(0, 0, 0, 0.19))";
+    "brightness(50%) sepia(1) hue-rotate(90deg) brightness(100%) saturate(200%) drop-shadow(0 0 50px rgba(0, 0, 0, 0.19))";
     document.getElementById("organic_shape_l1").style.filter =
-        "brightness(50%) sepia(1) hue-rotate(90deg) brightness(100%) saturate(200%) drop-shadow(0 0 50px rgba(0, 0, 0, 0.19))";
+    "brightness(50%) sepia(1) hue-rotate(90deg) brightness(120%) saturate(200%) drop-shadow(0 0 50px rgba(0, 0, 0, 0.19))";
     document.getElementById("organic_shape_r3").style.filter =
-        "brightness(50%) sepia(1) hue-rotate(90deg) brightness(60%) saturate(200%) drop-shadow(0 0 50px rgba(0, 0, 0, 0.19))";
+        "brightness(50%) sepia(1) hue-rotate(90deg) brightness(80%) saturate(180%) drop-shadow(0 0 50px rgba(0, 0, 0, 0.19))";
     document.getElementById("organic_shape_r2").style.filter =
-        "brightness(50%) sepia(1) hue-rotate(90deg) brightness(80%) saturate(200%) drop-shadow(0 0 50px rgba(0, 0, 0, 0.19))";
-    document.getElementById("organic_shape_r1").style.filter =
         "brightness(50%) sepia(1) hue-rotate(90deg) brightness(100%) saturate(200%) drop-shadow(0 0 50px rgba(0, 0, 0, 0.19))";
+    document.getElementById("organic_shape_r1").style.filter =
+        "brightness(50%) sepia(1) hue-rotate(90deg) brightness(120%) saturate(200%) drop-shadow(0 0 50px rgba(0, 0, 0, 0.19))";
+    
     sheet.innerHTML = "";
-    sheet.innerHTML += "* {--main-bg-color: #65da91;}\n";
-    sheet.innerHTML += "* {--main-text-color: #ffffff;}\n";
-    sheet.innerHTML += "#logo {filter: brightness(100000%)}\n";
+    sheet.innerHTML += "* {--main-bg-color: #ffffff;}\n";
+    sheet.innerHTML += "* {--logo-bg-color: #ffffff;}\n";
+    sheet.innerHTML += "* {--drop-shadow-color: #00b46f;}\n";
+    sheet.innerHTML += "* {--main-text-color: #000000;}\n";
+    sheet.innerHTML += "* {--secondary-text-color: #999999;}\n";
+    sheet.innerHTML += "* {--sidebar-text-color-highlighted: #ffffff;}\n";
+    sheet.innerHTML += "* {--sidebar-text-color: #ffffff;}\n";
     document.body.appendChild(sheet);
 }

9e9cfed37d1371fddd381f87663f81cfee39ee06
Maxime Tschopp <maxime.tschopp@icloud.com>
2022-09-27 21:59:28 +0200

    updated st patricks

--- color_management.js
+++ color_management.js
@@ -8,7 +8,7 @@ if (colortype == null) {
         colortype = 3; // Pride Month
     } else if (dueDay > 10 && dueMonth == 10) {
         colortype = 4; // Halloween
-    } else if (dueMonth == 12 && dueDay <= 1) {
+    } else if (dueMonth == 12 && dueDay <= 25) {
         colortype = 5; // Christmas
     } else {
         colortype = 0; // Default
@@ -41,17 +41,17 @@ function stPatricksTheme() {
 
     var sheet = document.createElement("style");
     document.getElementById("organic_shape_l3").style.filter =
-        "brightness(50%) sepia(1) hue-rotate(90deg) brightness(80%) saturate(180%) drop-shadow(0 0 50px rgba(0, 0, 0, 0.19))";
+        "brightness(50%) sepia(1) hue-rotate(90deg) brightness(80%) saturate(180%) drop-shadow(rgba(0, 191, 105, 1) 0px 0px 50px)";
     document.getElementById("organic_shape_l2").style.filter =
-        "brightness(50%) sepia(1) hue-rotate(90deg) brightness(100%) saturate(200%) drop-shadow(0 0 50px rgba(0, 0, 0, 0.19))";
+        "brightness(50%) sepia(1) hue-rotate(90deg) brightness(100%) saturate(200%) drop-shadow(rgba(0, 255, 140, 1) 0px 0px 50px)";
     document.getElementById("organic_shape_l1").style.filter =
-        "brightness(50%) sepia(1) hue-rotate(90deg) brightness(120%) saturate(200%) drop-shadow(0 0 50px rgba(0, 0, 0, 0.19))";
+        "brightness(50%) sepia(1) hue-rotate(90deg) brightness(120%) saturate(200%) drop-shadow(rgba(0, 255, 140, 1) 0px 0px 50px)";
     document.getElementById("organic_shape_r3").style.filter =
-        "brightness(50%) sepia(1) hue-rotate(90deg) brightness(80%) saturate(180%) drop-shadow(0 0 50px rgba(0, 0, 0, 0.19))";
+        "brightness(50%) sepia(1) hue-rotate(90deg) brightness(80%) saturate(180%) drop-shadow(rgba(0, 191, 105, 1) 0px 0px 50px)";
     document.getElementById("organic_shape_r2").style.filter =
-        "brightness(50%) sepia(1) hue-rotate(90deg) brightness(100%) saturate(200%) drop-shadow(0 0 50px rgba(0, 0, 0, 0.19))";
+        "brightness(50%) sepia(1) hue-rotate(90deg) brightness(100%) saturate(200%) drop-shadow(rgba(0, 255, 140, 1) 0px 0px 50px)";
     document.getElementById("organic_shape_r1").style.filter =
-        "brightness(50%) sepia(1) hue-rotate(90deg) brightness(120%) saturate(200%) drop-shadow(0 0 50px rgba(0, 0, 0, 0.19))";
+        "brightness(50%) sepia(1) hue-rotate(90deg) brightness(120%) saturate(200%) drop-shadow(rgba(0, 255, 140, 1) 0px 0px 50px)";
 
     sheet.innerHTML = "";
     sheet.innerHTML += "* {--main-bg-color: #ffffff;}\n";
@@ -107,16 +107,16 @@ function chineseNewYearTheme() {
     document.getElementById("organic_shape_l2").style.filter =
     "brightness(50%) sepia(1) saturate(10000%) brightness(70%)  drop-shadow(0 0 50px rgba(0, 0, 0, 0.19))";
     document.getElementById("organic_shape_l1").style.filter =
-    "brightness(50%) sepia(1) saturate(10000%) brightness(90%) drop-shadow(0 0 50px rgba(0, 0, 0, 0.19))";
+    "brightness(50%) sepia(1) saturate(10000%) saturate(40%) brightness(150%) drop-shadow(0 0 50px rgba(0, 0, 0, 0.19))";
     document.getElementById("organic_shape_r3").style.filter =
     "brightness(50%) sepia(1) saturate(10000%) brightness(60%) drop-shadow(0 0 50px rgba(0, 0, 0, 0.19))";
     document.getElementById("organic_shape_r2").style.filter =
     "brightness(50%) sepia(1) saturate(10000%) brightness(70%)  drop-shadow(0 0 50px rgba(0, 0, 0, 0.19))";
     document.getElementById("organic_shape_r1").style.filter =
-    "brightness(50%) sepia(1) saturate(10000%) brightness(90%) drop-shadow(0 0 50px rgba(0, 0, 0, 0.19))";
+    "brightness(50%) sepia(1) saturate(10000%) saturate(40%) brightness(150%) drop-shadow(0 0 50px rgba(0, 0, 0, 0.19))";
 
     sheet.innerHTML = "";
-    sheet.innerHTML += "* {--main-bg-color: ##870000;}\n";
+    sheet.innerHTML += "* {--main-bg-color: #fff;}\n";
     sheet.innerHTML += "* {--logo-bg-color: #ffffff;}\n";
     sheet.innerHTML += "* {--drop-shadow-color: #f9ed78;}\n";
     sheet.innerHTML += "* {--main-text-color: #000000;}\n";
@@ -134,22 +134,22 @@ function christmasTheme(){
 
     var sheet = document.createElement("style");
     document.getElementById("organic_shape_l3").style.filter =
-    "brightness(50%) sepia(1) saturate(10000%) brightness(60%) drop-shadow(0 0 50px rgba(0, 0, 0, 0.19))";
+        "brightness(50%) sepia(1) hue-rotate(90deg) brightness(80%) saturate(180%) drop-shadow(0 0 50px rgba(0, 0, 0, 0.19))";
     document.getElementById("organic_shape_l2").style.filter =
-    "brightness(50%) sepia(1) saturate(10000%) brightness(70%)  drop-shadow(0 0 50px rgba(0, 0, 0, 0.19))";
+        "brightness(50%) sepia(1) hue-rotate(90deg) brightness(100%) saturate(200%) drop-shadow(0 0 50px rgba(0, 0, 0, 0.19))";
     document.getElementById("organic_shape_l1").style.filter =
-    "brightness(50%) sepia(1) saturate(10000%) brightness(90%) drop-shadow(0 0 50px rgba(0, 0, 0, 0.19))";
+        "brightness(50%) sepia(1) hue-rotate(90deg) brightness(120%) saturate(200%) drop-shadow(0 0 50px rgba(0, 0, 0, 0.19))";
     document.getElementById("organic_shape_r3").style.filter =
-    "brightness(50%) sepia(1) saturate(10000%) brightness(60%) drop-shadow(0 0 50px rgba(0, 0, 0, 0.19))";
+        "brightness(50%) sepia(1) hue-rotate(90deg) brightness(80%) saturate(180%) drop-shadow(0 0 50px rgba(0, 0, 0, 0.19))";
     document.getElementById("organic_shape_r2").style.filter =
-    "brightness(50%) sepia(1) saturate(10000%) brightness(70%)  drop-shadow(0 0 50px rgba(0, 0, 0, 0.19))";
+        "brightness(50%) sepia(1) hue-rotate(90deg) brightness(100%) saturate(200%) drop-shadow(0 0 50px rgba(0, 0, 0, 0.19))";
     document.getElementById("organic_shape_r1").style.filter =
-    "brightness(50%) sepia(1) saturate(10000%) brightness(90%) drop-shadow(0 0 50px rgba(0, 0, 0, 0.19))";
+        "brightness(50%) sepia(1) hue-rotate(90deg) brightness(120%) saturate(200%) drop-shadow(0 0 50px rgba(0, 0, 0, 0.19))";
 
     sheet.innerHTML = "";
-    sheet.innerHTML += "* {--main-bg-color: ##870000;}\n";
+    sheet.innerHTML += "* {--main-bg-color: #fff;}\n";
     sheet.innerHTML += "* {--logo-bg-color: #ffffff;}\n";
-    sheet.innerHTML += "* {--drop-shadow-color: #f9ed78;}\n";
+    sheet.innerHTML += "* {--drop-shadow-color: #ff0000;}\n";
     sheet.innerHTML += "* {--main-text-color: #000000;}\n";
     sheet.innerHTML += "* {--secondary-text-color: #999999;}\n";
     sheet.innerHTML += "* {--sidebar-text-color-highlighted: #ffffff;}\n";

a10195acd568631b13d3cb9538462913afe17c27
Maxime Tschopp <maxime.tschopp@icloud.com>
2022-10-24 12:29:11 +0200

    share button works???

---
+++ NEW_UI/nm.js
@@ -0,0 +1,22 @@
+function share(Title, Url){
+
+    console.log("clicked");
+    
+    if(Title = null){
+        Title = "Rick Roll";
+    }
+    if(Url = null){
+        Url = "https://youtu.be/dQw4w9WgXcQ";
+    }
+
+    if (navigator.share) {
+        // Web Share API is supported
+        navigator.share({
+            title: Title,
+            url: Url
+          })
+      } else {
+        // Fallback
+        console.error("navigator.share is not supported");
+      }
+}

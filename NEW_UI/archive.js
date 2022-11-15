33365286f6fee1cc64b4c3d02b0479c90f8fb529
Maxime Tschopp <maxime.tschopp@icloud.com>
2022-11-15 21:05:43 +0100

    sidebar stuffs

---
+++ NEW_UI/archive.js
@@ -0,0 +1,34 @@
+function timelineScrollTo(timelineDate, offset){
+
+    if (typeof offset != 'number'){
+        let viewportHeight = window.innerHeight;
+        offset = -(viewportHeight * 0.1); // scroll to 10% above the item
+    }
+
+    console.log(timelineDate.className.search('thing-'));
+    var j = timelineDate.className.search('thing-'); // index of where the className starts
+    var i = j + 6;
+    var c = "thing-";
+    while (i < timelineDate.className.length && timelineDate.className.charAt(i+1) != " "){
+        c += timelineDate.className.charAt(i);
+        i++;
+    }
+    console.log(c); 
+
+    var item = document.getElementById(c);
+    var itemY = item.getBoundingClientRect().top + window.scrollY;
+
+    window.scroll({
+        top: itemY + offset,
+        left: 0,
+        behavior: 'smooth'
+    });
+
+    if(item.classList.contains('popAnim')){
+        console.log('yuh');
+        item.classList.remove('popAnim');
+        setTimeout( () => {item.classList.add('popAnim')}, 10);
+    } else {
+        item.classList.add('popAnim');
+    }
+}

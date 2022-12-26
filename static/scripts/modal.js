c86e19ce76b2845e053a1de18f7c285e61e348fb
Maxime Tschopp <maxime.tschopp@icloud.com>
2022-12-27 00:46:50 +0100

    modal v2

--- static/scripts/modal.js
+++ static/scripts/modal.js
@@ -7,5 +7,8 @@ function toggleModal(override) {
             document.getElementById('modal-container').classList.add('modal-container-hidden');
             document.getElementById('modal').classList.add('modal-hidden');
         }
-    } 
+    } else {
+        document.getElementById('modal-container').classList.toggle('modal-container-hidden');
+        document.getElementById('modal').classList.toggle('modal-hidden');
+    }
 }

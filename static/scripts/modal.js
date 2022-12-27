38bdb62dbb326ade01743c879e29106b7c7c9552
Maxime Tschopp <maxime.tschopp@icloud.com>
2022-12-27 14:11:41 +0100

    modal done??

--- static/scripts/modal.js
+++ static/scripts/modal.js
@@ -1,14 +1,38 @@
+
+addEventListener("resize", (event) => {
+    updateVideoPlayerSizes();
+});
+
+
 function toggleModal(override) {
+    setTimeout(updateVideoPlayerSizes, 100);
     if (override !== undefined) {
         if(override){
             document.getElementById('modal-container').classList.remove('modal-container-hidden');
             document.getElementById('modal').classList.remove('modal-hidden');
+            document.body.style.overflowY = 'hidden';
         } else {
             document.getElementById('modal-container').classList.add('modal-container-hidden');
             document.getElementById('modal').classList.add('modal-hidden');
+            document.body.style.overflowY = 'auto';
         }
     } else {
         document.getElementById('modal-container').classList.toggle('modal-container-hidden');
         document.getElementById('modal').classList.toggle('modal-hidden');
+        if(document.getElementById('modal-container').classList.contains('modal-container-hidden')){
+            document.body.style.overflowY = 'auto';
+        } else {
+            document.body.style.overflowY = 'hidden';
+        }
     }
+}
+function updateVideoPlayerSizes(){
+    for(let i = 0; i < document.getElementsByClassName('modal-video').length; i++){
+        let videoplayer = document.getElementsByClassName('modal-video')[i].children[0];
+        let w = document.getElementById('modal').offsetWidth;
+        let h = (w/16) * 9;
+        videoplayer.setAttribute('width', w);
+        videoplayer.setAttribute('height', h);
+    }
+    
 }

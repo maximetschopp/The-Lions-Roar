1a32261dc6943b84e5417f26fc6a9d964b9a1bfd
Maxime Tschopp <maxime.tschopp@icloud.com>
2022-12-28 19:27:58 +0100

    editor v2

--- static/scripts/editor.js
+++ static/scripts/editor.js
@@ -1,50 +1,12 @@
-var dragging = false;
-var dragBarSelected;
-var panelMinWidth = 250;
-var panelMaxWidth = 900;
+addEventListener('resize', setbodyWidth);
 
 
-addEventListener('DOMContentLoaded', (event) => {ready()});
-
-function dragstart(e, dragbarNumber) {
-  e.preventDefault();
-  dragBarSelected = dragbarNumber;
-  dragging = true;
-}
-function dragmove(e) {
-  if (dragging) 
-  {  
-    var percentage = ((e.pageX-document.getElementsByClassName("panel")[dragBarSelected].getBoundingClientRect()['x']) / window.innerWidth) * 100;
-    console.log(e.pageX);
-    if (percentage > 5 && percentage < 98) {
-      document.getElementsByClassName("panel")[dragBarSelected].style.width = percentage + "%";
+function setbodyWidth(){
+    let totalWidth = 0;
+    for(let i=0; i < document.getElementsByClassName('panel').length; i++){
+        totalWidth += document.getElementsByClassName('panel')[i].offsetWidth;
     }
-
-  }
-}
-function dragend() {
-  dragging = false;
-  dragBarSelected = -1;
-  var vend = navigator.vendor;
-  if (window.editor && vend.indexOf("Apple") == -1) {
-      window.editor.refresh();
-  }
-}
-
-function ready() {
-
-  if (window.addEventListener) {       
-    
-    for(let i = 0; i < document.getElementsByClassName('dragbar').length; i++) {
-      document.getElementsByClassName('dragbar')[i].addEventListener("mousedown", function(e) {dragstart(e, i);});
-      document.getElementsByClassName('dragbar')[i].addEventListener("touchstart", function(e) {dragstart(e, i);});
-
-    }
-    
-      window.addEventListener("mousemove", function(e) {dragmove(e);});
-      window.addEventListener("touchmove", function(e) {dragmove(e);});
-      window.addEventListener("mouseup", dragend);  
-      window.addEventListener("touchend", dragend);
-  }
-
+    totalWidth = Math.max(width, window.innerWidth);
+    document.body.style.width = totalWidth + 'px';
+    console.log(totalWidth);
 }

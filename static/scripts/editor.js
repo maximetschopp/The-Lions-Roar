c4a8b9d1797edd6364a7e7f9c4be4f70af699fa4
Maxime Tschopp <maxime.tschopp@icloud.com>
2022-12-28 21:50:45 +0100

    editor v1

--- static/scripts/editor.js
+++ static/scripts/editor.js
@@ -1,12 +1,5 @@
-addEventListener('resize', setbodyWidth);
+addEventListener('resize', updateTables);
 
-
-function setbodyWidth(){
-    let totalWidth = 0;
-    for(let i=0; i < document.getElementsByClassName('panel').length; i++){
-        totalWidth += document.getElementsByClassName('panel')[i].offsetWidth;
-    }
-    totalWidth = Math.max(width, window.innerWidth);
-    document.body.style.width = totalWidth + 'px';
-    console.log(totalWidth);
+function updateTables(){
+    
 }
